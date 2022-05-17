1c4bfbf4b4e25589f4f821d5d2ed73f3f8a6c1ed
Partha Pratim Sen <partha1189@gmail.com>
2022-05-17 10:19:07 +0530

    Create BFT.js

---
+++ BFT.js
@@ -0,0 +1,55 @@
+class Node {
+	constructor(val){
+  	this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const a = new Node('a'); 
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+
+			/* 				a
+				       /  \
+				      b    c
+				     /  \    \
+				    d    e     f */
+            
+            
+
+/* Iterative BFT */
+
+const breadthFirstValues = (root) => {
+	if(root === null){
+		return [];
+	}
+  const queue = [root];
+  const result = [];
+  
+  while(queue.length > 0){
+  	const current = queue.shift();
+    result.push(current.val);
+    if(current.left !== null){
+    	queue.push(current.left);
+    }
+  	if(current.right !== null){
+    	queue.push(current.right);
+    }
+	}
+  
+  return result;
+}
+console.log(breadthFirstValues(a));
+
